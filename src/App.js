5b75a8bc658dd1c15d9881f540d75354f0918027
Satnam <satnam10virdi@gmail.com>
2021-01-15 14:18:35 +0530

    Fixed weather card and added error handling

--- src/App.js
+++ src/App.js
@@ -1,40 +1,59 @@
 import { useState } from "react";
 function App() {
   const [weatherData, setWeatherData] = useState("");
+  const [error, setError] = useState("");
 
   const fetchDataByCity = async (cityName) => {
     const apiKey = "7b130bb6b2e00f735a3023b3b5e375bb";
     const fetchURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-    console.log("FETCH CALLED!");
+
+    // reset state data before fetch
+    setWeatherData("");
+    setError(null);
+
     const fetchedData = await fetch(fetchURL)
       .then((response) => response.json())
-      .then((data) => data)
-      .catch((err) => console.log(err));
+      .then((data) => data);
 
-    setWeatherData(fetchedData);
-  };
-
-  const searchCity = (cityName) => {
-    fetchDataByCity(cityName);
+    if (fetchedData.cod === 200) {
+      setWeatherData(fetchedData);
+      setError(null);
+    } else {
+      setError({
+        cod: fetchedData.cod,
+        message: fetchedData.message,
+      });
+      console.warn(`${fetchedData.cod}: ${fetchedData.message}`);
+    }
   };
 
   return (
     <div className="App">
-      <SearchWidget searchCity={searchCity} />
-      <WeatherCard weatherData={weatherData} />
+      <SearchWidget fetchDataByCity={fetchDataByCity} />
+      {error ? (
+        <WeatherCard weatherData={weatherData} error={error} />
+      ) : (
+        <WeatherCard weatherData={weatherData} />
+      )}
     </div>
   );
 }
 
-const SearchWidget = ({ searchCity }) => {
-  const [cityName, setCityName] = useState("New York");
+// //
+
+const SearchWidget = ({ fetchDataByCity }) => {
+  const [cityName, setCityName] = useState("");
   const handleCityChange = (e) => {
     setCityName(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    searchCity(cityName);
+    if (cityName.trim()) {
+      fetchDataByCity(cityName);
+    } else {
+      alert("Please type city name in the search box");
+    }
   };
 
   return (
@@ -42,7 +61,7 @@ const SearchWidget = ({ searchCity }) => {
       <form onSubmit={handleSubmit}>
         <input
           type="text"
-          placeholder="Type location here"
+          placeholder="Type location here, e.g. New York"
           onChange={handleCityChange}
           value={cityName}
         />
@@ -52,45 +71,65 @@ const SearchWidget = ({ searchCity }) => {
   );
 };
 
-const WeatherCard = ({ weatherData }) => {
-  console.log(weatherData);
+// //
 
-  // TODO: only show data when weatherData.cod is 200
+const WeatherCard = ({ weatherData, error }) => {
+  let cardData;
+
+  if (weatherData) {
+    cardData = <div>has data</div>;
+  } else {
+    if (error) {
+      cardData = <p>{`${error.cod}: ${error.message}`}</p>;
+    } else {
+      cardData = <p>no data, no errors</p>;
+    }
+  }
 
   return (
     <>
-      <h4>{weatherData ? "Loaded" : "Loading..."}</h4>
-      <div>
-        {weatherData ? (
-          <div>
-            <p>{weatherData ? weatherData?.name : "loading..."}</p>
-            <p>{weatherData ? weatherData.coord?.lat : " loading..."}</p>
-            <p>{weatherData ? weatherData.coord?.lon : " loading..."}</p>
-            <p>{weatherData ? weatherData.main?.feels_like : " loading..."}</p>
-            <p>{weatherData ? weatherData.main?.temp : " loading..."}</p>
-            <p>{weatherData ? weatherData.main?.temp_max : " loading..."}</p>
-            <p>{weatherData ? weatherData.main?.temp_min : " loading..."}</p>
-            <p>{weatherData ? weatherData.main?.pressure : " loading..."}</p>
-            <p>{weatherData ? weatherData.main?.humidity : " loading..."}</p>
-            <p>{weatherData ? weatherData.sys?.country : " loading..."}</p>
-            <p>{weatherData ? weatherData.sys?.sunrise : " loading..."}</p>
-            <p>{weatherData ? weatherData.sys?.sunset : " loading..."}</p>
-            <p>{weatherData ? weatherData?.timezone : " loading..."}</p>
-            <p>{weatherData ? weatherData?.visibility : " loading..."}</p>
-            <p>
-              {weatherData
-                ? weatherData.weather[0]?.description
-                : " loading..."}
-            </p>
-            <p>{weatherData ? weatherData.weather[0]?.main : " loading..."}</p>
-            <p>{weatherData ? weatherData.weather[0]?.icon : " loading..."}</p>
-            <p>{weatherData ? weatherData.wind?.degree : " loading..."}</p>
-            <p>{weatherData ? weatherData.wind?.speed : " loading..."}</p>
-          </div>
-        ) : (
-          "No weather data yet..."
-        )}
-      </div>
+      <h3>Card Data </h3>
+      <div>{cardData}</div>
+      <section className="hidden">
+        <h4>Test DIV below</h4>
+        <div>
+          {weatherData ? (
+            <div>
+              <p>{weatherData ? weatherData?.name : "loading..."}</p>
+              <p>{weatherData ? weatherData.coord?.lat : " loading..."}</p>
+              <p>{weatherData ? weatherData.coord?.lon : " loading..."}</p>
+              <p>
+                {weatherData ? weatherData.main?.feels_like : " loading..."}
+              </p>
+              <p>{weatherData ? weatherData.main?.temp : " loading..."}</p>
+              <p>{weatherData ? weatherData.main?.temp_max : " loading..."}</p>
+              <p>{weatherData ? weatherData.main?.temp_min : " loading..."}</p>
+              <p>{weatherData ? weatherData.main?.pressure : " loading..."}</p>
+              <p>{weatherData ? weatherData.main?.humidity : " loading..."}</p>
+              <p>{weatherData ? weatherData.sys?.country : " loading..."}</p>
+              <p>{weatherData ? weatherData.sys?.sunrise : " loading..."}</p>
+              <p>{weatherData ? weatherData.sys?.sunset : " loading..."}</p>
+              <p>{weatherData ? weatherData?.timezone : " loading..."}</p>
+              <p>{weatherData ? weatherData?.visibility : " loading..."}</p>
+              <p>
+                {weatherData
+                  ? weatherData.weather[0]?.description
+                  : " loading..."}
+              </p>
+              <p>
+                {weatherData ? weatherData.weather[0]?.main : " loading..."}
+              </p>
+              <p>
+                {weatherData ? weatherData.weather[0]?.icon : " loading..."}
+              </p>
+              <p>{weatherData ? weatherData.wind?.degree : " loading..."}</p>
+              <p>{weatherData ? weatherData.wind?.speed : " loading..."}</p>
+            </div>
+          ) : (
+            "No weather data yet..."
+          )}
+        </div>
+      </section>
     </>
   );
 };
